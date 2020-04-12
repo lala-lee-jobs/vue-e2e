a0e1d6afefb35c05d2ccd94d49d67d89813cd8f1
拉拉醬 <lala.lee.goo@gmail.com>
2020-04-12 19:33:30 +0800

    HellowWorld修改故意錯

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="home">
     <img alt="Vue logo" src="../assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App aaa"/>
+    <HelloWorld msg="Hello world"/>
     <p>Deploy by github actions</p>
   </div>
 </template>
